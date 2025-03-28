f58cc1c21965f22fba5c7bad2b828071237fc1bc
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-28 09:32:55 -0600

    fix: convexclientprovider big fixed - changed file type to tsx instead of ts

--- comms-app/convex/auth.config.ts
+++ comms-app/convex/auth.config.ts
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://your-issuer-url.clerk.accounts.dev/",
-      applicationID: "convex",
+      domain: 'https://fair-elephant-13.clerk.accounts.dev',
+      applicationID: 'convex',
     },
-  ]
-};
+  ],
+}
---
+++ comms-app/providers/ConvexClientProvider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ConvexReactClient } from 'convex/react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL;
+const CLERK_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!CONVEX_URL || !CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing environment variables for Convex or Clerk.");
+}
+
+const convex = new ConvexReactClient(CONVEX_URL);
+
+const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default ConvexClientProvider;
