0a97c272193243d4f4f8e6ee7846fe81174bb8f0
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-28 10:14:40 -0600

    add: loading screen and

--- comms-app/app/layout.tsx
+++ comms-app/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/providers/ConvexClientProvider";
+import { Authenticated, AuthLoading } from "convex/react";
+import LoadingLogo from "@/components/shared/LoadingLogo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
