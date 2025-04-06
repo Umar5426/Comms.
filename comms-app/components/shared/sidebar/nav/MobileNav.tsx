92fc4c42718eb94c5f8dd8cedd694a163fd4e121
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-05 22:47:44 -0600

    fix: styling update

--- comms-app/components/shared/sidebar/nav/MobileNav.tsx
+++ comms-app/components/shared/sidebar/nav/MobileNav.tsx
@@ -5,40 +5,38 @@ import { useNavigation } from '@/hooks/useNavigation';
 import { UserButton } from '@clerk/clerk-react';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 const MobileNav = () => {
     const paths = useNavigation();
 
     return (
-        <Card className="hidden lg:flex lg:flex-col 
-            lg:justify-between lg:items-center lg:h-full
-            lg:w-16 lg:px-2 lg:py-4">
-            <nav>
-                <ul className='flex flex-col items-center gap-4'>
-                    {paths.map((path, id) => {
-                        return <li key={id} className='relative'>
+        <Card className="fixed bottom-4 ">
+            <nav className="w-full">
+                <ul className="flex justify-between items-center gap-2">
+                    {paths.map((path, id) => (
+                        <li key={id} className="relative">
                             <Link href={path.href}>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <Button size='icon' variant={path.active ? "default" : "outline"}>
+                                        <Button size="icon" variant={path.active ? "default" : "outline"}>
                                             {path.icon}
                                         </Button>
                                     </TooltipTrigger>
-                                    <TooltipContent>
+                                    <TooltipContent side="top">
                                         <p>{path.name}</p>
                                     </TooltipContent>
                                 </Tooltip>
                             </Link>
                         </li>
-                    })}
+                    ))}
+                    <li>
+                        <UserButton />
+                    </li>
                 </ul>
             </nav>
-            <div className='flex flex-col items-center gap-4'>
-                <UserButton/>
-            </div>
         </Card>
     );
-}
+};
 
 export default MobileNav;
