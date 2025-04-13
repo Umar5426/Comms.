970a4a92460ae6581349b46f5c90cfc551578472
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-13 17:06:10 -0600

    fix: adding fallback page to friends page

--- comms-app/app/(root)/friends/page.tsx
+++ comms-app/app/(root)/friends/page.tsx
@@ -1,3 +1,4 @@
+import ConversationFallback from '@/components/shared/conversation/ConversationFallback';
 import ItemList from '@/components/shared/item-list/ItemList';
 import React from 'react'
 
@@ -5,9 +6,10 @@ type Props = {};
 
 const FriendsPage = (props: Props) => {
   return (
-    <div>
+    <>
       <ItemList title="Friends">Friends Page</ItemList>
-    </div>
+      <ConversationFallback/>
+    </>
   )
 }
 
