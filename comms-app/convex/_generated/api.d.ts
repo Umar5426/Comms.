8579da90c6a2ccca0d20f9fa651a11ac175ec8e0
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-13 18:23:19 -0600

    add: adding a request comp to handle requests

--- comms-app/convex/_generated/api.d.ts
+++ comms-app/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as http from "../http.js";
+import type * as request from "../request.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  request: typeof request;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
