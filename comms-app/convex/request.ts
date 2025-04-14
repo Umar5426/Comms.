f1482042698405a9b93e387351cc9fecdd7dfda1
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-14 16:17:32 -0600

    add: reciver func to requests

--- comms-app/convex/request.ts
+++ comms-app/convex/request.ts
@@ -1,5 +1,6 @@
 import { ConvexError, v } from "convex/values";
 import { mutation } from "./_generated/server";
+import { getUserByClerkId } from "./_utils";
 
 export const create = mutation({
     args: {
@@ -15,5 +16,21 @@ export const create = mutation({
         if(args.email === identity.email) {
             throw new ConvexError("You cannot send a request to yourself");
         }
+
+        const currentUser = await getUserByClerkId({
+            ctx, clerkId: identity.subject
+        })
+        
+    if(!currentUser) {
+        throw new ConvexError("User not found")
     }
+
+    const reciever = await ctx.db
+        .query("user")
+        .withIndex("by_email", (q) => q.eq
+        ("email", args.email))
+        .unique()
+
+        const
+}
 })
