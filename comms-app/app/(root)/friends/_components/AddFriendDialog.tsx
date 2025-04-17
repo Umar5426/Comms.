a8aa8368eea9c421210b0882f6452184f14bc9e1
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-16 20:02:48 -0600

    add: adding dailog return

--- comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
+++ comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import React from 'react'
 import { z } from 'zod';
 import {zodResolver} from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { Tooltip, TooltipTrigger } from '@radix-ui/react-tooltip';
+import { Button } from '@/components/ui/button';
+import { UserPlus } from 'lucide-react';
+import {
+    Dialog,
+    DialogTrigger,
+  } from "@/components/ui/dialog";
 
 type Props = {}
 
@@ -13,7 +22,18 @@ const AddFriendDialog = (props: Props) => {
 
     const form = useForm<z.infer<typeof addFriendFormSchema>>({resolver: zodResolver(addFriendFormSchema), defaultValues: {email: ''}});
   return (
-    <div>AddFriendDialog</div>
+    <Dialog>
+        <Tooltip>
+            <TooltipTrigger>
+                <Button size="icon" variant={'outline'}>
+                    <DialogTrigger>
+
+                        <UserPlus/>
+                    </DialogTrigger>
+                </Button>
+            </TooltipTrigger>
+        </Tooltip>
+    </Dialog>
   )
 }
 
