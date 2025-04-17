a23509a45b2f9487b8d22949ee814cd7b645e100
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-16 19:47:38 -0600

    add: adding action logic

--- comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
+++ comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
@@ -4,9 +4,13 @@ import {zodResolver} from '@hookform/resolvers/zod';
 
 type Props = {}
 
-const addFriendFormSchema = z.object;
+const addFriendFormSchema = z.object({
+    email: z.string().min(1, {message: 'Invalid email address'}).email({}),
+});
 
 const AddFriendDialog = (props: Props) => {
+
+    const form = useForm<z.infer<typeof addFriendFormSchema>>({resolver: zodResolver(addFriendFormSchema), defaultValues: {email: ''}});
   return (
     <div>AddFriendDialog</div>
   )
