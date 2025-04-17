31a76690a29f7501ec019524fbbe7408e0b980f5
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-16 21:08:18 -0600

    add: adding submit logic, and updating imports

--- comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
+++ comms-app/app/(root)/friends/_components/AddFriendDialog.tsx
@@ -18,35 +18,44 @@ import {
 import { DialogDescription } from '@radix-ui/react-dialog';
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { useMutationState } from '@/hooks/useMutationState';
+import { api } from '@/convex/_generated/api';
+import { toast } from 'sonner';
+import { ConvexError } from 'convex/values';
 
-
-type Props = {}
 
 const addFriendFormSchema = z.object({
     email: z.string().min(1, {message: 'Invalid email address'}).email({}),
 });
 
-const AddFriendDialog = (props: Props) => {
+const AddFriendDialog = () => {
+
+    const {mutate: createRequest, pending} = useMutationState(api.request.create);
 
     const form = useForm<z.infer<typeof addFriendFormSchema>>({resolver: zodResolver(addFriendFormSchema), defaultValues: {email: ''}});
 
-    const handleSubmit = async (data: z.infer<typeof addFriendFormSchema>) => {
-        console.log(data);
+    const handleSubmit = async (values: z.infer<typeof addFriendFormSchema>) => {
+        await createRequest({email: values.email}).then(() => {
+            form.reset();
+            toast.success('Request sent successfully');
+        }).catch((error) => {
+            toast.error(error instanceof ConvexError ? error.message : 'Something went wrong');
+        })
     }
   return (
-        <Dialog>
-        <Tooltip>
-            <DialogTrigger asChild>
+    <Dialog>
+        <DialogTrigger asChild>
+            <Tooltip>
             <TooltipTrigger asChild>
                 <Button size="icon" variant="outline">
                 <UserPlus />
                 </Button>
             </TooltipTrigger>
-            </DialogTrigger>
             <TooltipContent>
-            <p>Add Friends</p>
+                <p>Add Friends</p>
             </TooltipContent>
-        </Tooltip>
+            </Tooltip>
+        </DialogTrigger>
 
 
         <DialogContent>
